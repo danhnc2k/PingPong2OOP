6e41520ac8f024ece353eedc103dee10587d548c
danhnc2k <55302957+danhnc2k@users.noreply.github.com>
2019-12-12 17:01:06 +0700

    Nhớ gọi hàm MoveItem trong ball.cs

--- Brick.cs
+++ Brick.cs
@@ -11,7 +11,7 @@ namespace PingPongGame
     class Brick
     {
         public PictureBox pBrick;
-        
+        public Item It;
         
         
         public void DrawBrick(int xPos,int yPos,Color vColor)
@@ -23,43 +23,49 @@ namespace PingPongGame
                 Location = new Point(xPos, yPos),
                 BackColor = vColor
             };
-            //pBrick.Visible = true;
+            It = new Item();
+            It.DrawItem(xPos+GlobalVariable.BrickWidth/2-GlobalVariable.BrickHeight/2, yPos, GlobalVariable.BrickHeight, GlobalVariable.BrickHeight);
         }
         public bool CheckCollison(Ball b)
         {
-            
+            if (pBrick.Visible == false)
+                return false;
             int xStep = b.xSpeed, yStep = b.ySpeed;
             if (xStep > b.pBall.Width)
                 xStep = b.pBall.Width;
             if (yStep > b.pBall.Height)
                 yStep = b.pBall.Height;
-            int top, bot, left, right;
+            int top, bot, left, right, xBall, yBall;
             top = pBrick.Top + GlobalVariable.BrickWallyPos;
             bot = pBrick.Bottom + GlobalVariable.BrickWallyPos;
             left = pBrick.Left + GlobalVariable.BrickWallxPos;
             right = pBrick.Right + GlobalVariable.BrickWallxPos;
-            if (b.pBall.Top +yStep < bot && b.pBall.Top > top && b.pBall.Location.X >= left && b.pBall.Location.X <= right)
+            xBall = b.pBall.Location.X + b.pBall.Width / 2;
+            yBall = b.pBall.Location.Y + b.pBall.Height / 2;
+            if (b.pBall.Left + xStep <= right && b.pBall.Left > left && yBall >= top && yBall <= bot)
+            {
+                b.xSpeed *= -1;
+                return true;
+            }
+            if (b.pBall.Right + xStep >= left && b.pBall.Right < right && yBall >= top && yBall <= bot)
+            {
+                b.xSpeed *= -1;
+                return true;
+            }
+            if (yStep<0 && b.pBall.Top +yStep <= bot && b.pBall.Top >= top &&xBall >= left && xBall <= right)
             {
                 b.ySpeed *= -1;
                 return true;
             }
                 
-            if (b.pBall.Bottom+yStep  > top && b.pBall.Bottom < bot && b.pBall.Location.X >= left && b.pBall.Location.X <= right)
+            if (yStep>0 && b.pBall.Bottom+yStep  >= top && b.pBall.Bottom <= bot && xBall >= left && xBall <= right)
             {
                 b.ySpeed *= -1;
                 return true;
             }
-            if (b.pBall.Left + xStep < right && b.pBall.Left > left && b.pBall.Location.Y > top && b.pBall.Location.Y < bot)
-            {
-                b.xSpeed *= -1;
-                return true;
-            }
-            if (b.pBall.Right + xStep > left && b.pBall.Right < right && b.pBall.Location.Y > top && b.pBall.Location.Y < bot)
-            {
-                b.xSpeed *= -1;
-                return true;
-            }
+            
             return false;
         }
+       
     }
 }
